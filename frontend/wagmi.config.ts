845a582dbf8fcb90643738f8cb06948c9bb465fc
Cam Sweeney <cpsweene@gmail.com>
2024-12-17 23:04:16 -0800

    fix chainid

--- frontend/wagmi.config.ts
+++ frontend/wagmi.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         TransparentUpgradeableProxy: {
           31337: "0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8",
           84532: "0x6367C37ABCb4726561E51c8eF1cE78A400badAdC",
-          84533: "0x6367C37ABCb4726561E51c8eF1cE78A400badAdC",
+          8453: "0x6367C37ABCb4726561E51c8eF1cE78A400badAdC",
         },
       },
     }),
